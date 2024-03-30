da31bf6d8ba0004ad3b33ee633863920b133720f
Zav141DJacob <jaagup.tomingas@gmail.com>
2024-03-30 11:26:17 +0200

    Rockets

--- Assets/Guns/RocketGun.cs
+++ Assets/Guns/RocketGun.cs
@@ -6,20 +6,4 @@ public class RocketGun : MonoBehaviour
 {
 
     public float radius = 2f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void Shoot()
-    {
-        // Shoot rocket projectile
-    }
 }
--- Assets/Scripts/weapons/BulletBehaviour.cs
+++ Assets/Scripts/weapons/BulletBehaviour.cs
@@ -16,7 +16,7 @@ public class BulletBehaviour : MonoBehaviour
 
 
     private Rigidbody2D rb;
-
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -65,10 +65,33 @@ public class BulletBehaviour : MonoBehaviour
             if (rigid != null)
             {
                 Debug.Log("Found component " + rigid.name);
-                rigid.AddTorque(addTorqueAmountInDegrees * Mathf.Deg2Rad * rigid.inertia);
+                //rigid.AddForce(new Vector2(0, 0));
+                rigid.AddExplosionForce(100, transform.position, 20);
+
+                //rigid.AddTorque(addTorqueAmountInDegrees * Mathf.Deg2Rad * rigid.inertia);
             }
         }
 
         Destroy(toRemove, .5f);
     }
+
+    //public static void AddExplosionForce(this Rigidbody2D rb, float explosionForce, Vector2 explosionPosition, float explosionRadius, float upwardsModifier = 0.0F, ForceMode2D mode = ForceMode2D.Force)
+    //{
+    //    var explosionDir = rb.position - explosionPosition;
+    //    var explosionDistance = explosionDir.magnitude;
+
+    //    // Normalize without computing magnitude again
+    //    if (upwardsModifier == 0)
+    //        explosionDir /= explosionDistance;
+    //    else
+    //    {
+    //        // From Rigidbody.AddExplosionForce doc:
+    //        // If you pass a non-zero value for the upwardsModifier parameter, the direction
+    //        // will be modified by subtracting that value from the Y component of the centre point.
+    //        explosionDir.y += upwardsModifier;
+    //        explosionDir.Normalize();
+    //    }
+
+    //    rb.AddForce(Mathf.Lerp(0, explosionForce, (1 - explosionDistance)) * explosionDir, mode);
+    //}
 }
