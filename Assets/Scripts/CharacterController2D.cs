25d5138de72fe7b404f77c34728160f18f3a0930
Joosep127 <48839946+Joosep127@users.noreply.github.com>
2024-03-30 14:24:38 +0200

    Fixed player movement (Wall collision is still broken)

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -2,43 +2,69 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb;
-    public float jumpForce = 20f;
+
+    private Vector2 velocity;
+    private BoxCollider2D boxCollider;
+    public bool grounded;
+    public bool wall;
+
+    public float groundDeceleration;
+    public float airAcceleration;
+    public float walkAcceleration;
+    public float jumpHeight = 20f;
     public float movementSpeed = 5f;
-
+    public float distanceDown = 0.6f;
+    public LayerMask collisionLayers;
+    private Rigidbody2D rb;
     void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        // Freeze rotation to prevent unwanted rotation
-        rb.freezeRotation = true;
+    {   
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        velocity = Vector2.zero;
     }
 
-    private void Update()
-    {
-        // Handle player movement
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        Vector3 movement = new Vector3(moveInput, 0f, 0f) * movementSpeed * Time.deltaTime;
-        transform.position += movement;
-
-        // Handle player jump
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
-    }
-
-    bool IsGrounded()
-    {
-        RaycastHit hit;
-        float raycastDistance = 0.1f; // Decreased raycast distance
-        // Raycast to the floor objects only
-        int mask = 1 << LayerMask.NameToLayer("Ground");
-
-        // Raycast downwards
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance, mask))
-        {
+    public bool is_ground() {
+        Vector2 player_pos = transform.position;
+        RaycastHit2D Ground_Check = Physics2D.Raycast(player_pos, Vector2.down, distanceDown, collisionLayers);     
+        if (Ground_Check.collider != null) {
             return true;
-        }
+        } 
         return false;
     }
+    private void Update()
+    {
+        float acceleration = grounded ? walkAcceleration : airAcceleration;
+        float deceleration = grounded ? groundDeceleration : 0;
+
+        transform.Translate(velocity * Time.deltaTime);
+        float moveInput = Input.GetAxisRaw("Horizontal");
+velocity.x = Mathf.MoveTowards(velocity.x, movementSpeed * moveInput, walkAcceleration * Time.deltaTime);
+
+        if (moveInput != 0)
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, movementSpeed * moveInput, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
+        }
+
+
+        grounded = is_ground();
+
+        if (grounded)
+        {
+            velocity.y = 0;
+
+            if (Input.GetButtonDown("Jump"))
+                {
+                    rb.velocity = Vector2.up * jumpHeight;
+                }
+        }
+        else {
+            velocity.y += Physics2D.gravity.y * Time.deltaTime;
+        }
+    }
+
+    
 }
