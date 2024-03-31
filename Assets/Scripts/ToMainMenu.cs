979c289f0a37b56fd1af90c5d63ece2af74bee46
Joosep127 <48839946+Joosep127@users.noreply.github.com>
2024-03-31 06:37:13 +0300

    Added old stuff back

---
+++ Assets/Scripts/ToMainMenu.cs
@@ -0,0 +1,20 @@
+@ -0,0 + 1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToMainMenu : MonoBehaviour
+{
+
+    public static SceneController Instance;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Enter new close trigger level player");
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Enter new level player");
+            StartCoroutine(SceneController.Instance.LoadLevel(0));
+        }
+    }
+}
