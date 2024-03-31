8e04007d9c4c3d7dfa01b15298a160ee56e19097
MarcusSaar <marcuss@tlu.ee>
2024-03-31 02:33:01 +0200

    Added some sounds to script Need to be able to play them

---
+++ Assets/Scripts/SoundEffectsPlayer.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class SoundEffectsPlayer : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    public AudioClip click;
+    public AudioClip slideEnd;
+    public AudioClip slideMiddle;
+    public AudioClip slideStart;
+    public AudioClip explosion;
+    public AudioClip fallsound;
+    public AudioClip jumpsound;
+    public AudioClip shootsound;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayClickSound()
+    {
+        audioSource.PlayOneShot(click);
+    }
+
+    // public static void PlaySound(AudioSource sound) {
+    //     GameObject soundGameObject = new GameObject("Sound");
+    //     AudioSource audioSource = soundGameObject.AddComponents<AudioSource>();
+    //     audioSource.PlayOneShot();
+    // }
+
+    // public AudioSource Sfx;
+    // public AudioClip jumpSound; // Assign this in the Inspector
+    // public AudioClip shootingSound1; // Assign this in the Inspector
+
+    // // Example: Play jump sound
+    // public void PlayJumpSound()
+    // {
+    //     Sfx.PlayOneShot(jumpSound);
+    // }
+
+    // // Example: Play shooting sound
+    // public void PlayShootingSound()
+    // {
+    //     Sfx.PlayOneShot(shootingSound1);
+    // }
+}
